644c2f32c757232d9cd5eea6e5ee7d98a00996e4
Eddy Oliveira Mesquita <eddy_eom@hotmail.com>
2024-09-25 09:29:57 -0300

    hotfix: .env and db connection

--- app/database.py
+++ app/database.py
@@ -1,11 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/blog_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não está definido no arquivo .env")
+
+print(f"URL do Banco de Dados: {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
